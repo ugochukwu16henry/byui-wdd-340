e44a7e89b5d041a5d6cd86bb8b60cf5ebc86b5be
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-06 21:41:39 +0100

    commit

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
