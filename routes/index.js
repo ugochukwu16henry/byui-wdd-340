241c8957695d54593f6e7407c2f0a9c2153abb7d
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-15 19:09:47 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
-// Inex route/static.js
-app.get("/", baseController.buildHome)
- 
+
+const express = require("express")
+const router = express.Router()
+const baseController = require("../controllers/baseController")
+
+// Define route
+router.get("/", baseController.buildHome)
+
+module.exports = router
--- server.js
+++ server.js
@@ -45,3 +45,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
