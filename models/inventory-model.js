956a881bb37c1cafb766b2093a02a94c2c6ba92c
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-20 06:24:02 +0100

    new commit

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,4 +1,4 @@
-const pool = require("../database/")
+const pool = require("../utils/database/")
 
 /**************************
  * Get all classification data
@@ -7,22 +7,47 @@ async function getClassifications() {
     return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
+/**
+ * Get a single inventory item by inv_id (parameterized for safety)
+ * @param {number} invId
+ * @returns {Promise<Object|null>}
+ */
+async function getInventoryByClassificationId(invId) {
+  const sql = `
+    SELECT
+      inv_id,
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id
+    FROM public.inventory
+    WHERE inv_id = $1
+    LIMIT 1;
+  `;
+
+  const params = [invId];
+
+  try {
+    const result = await db.query(sql, params);
+    if (result.rows.length) {
+      return result.rows[0];
+    }
+    return null;
+  } catch (err) {
+    // bubble up the error to controller to be handled by middleware
+    throw err;
+  }
+}
+
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
  * ************************** */
-async function getInventoryByClassificationId(classification_id) {
-  try {
-    const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
-      [classification_id]
-    )
-    return data.rows,
-  } catch (error) {
-    console.error("getclassificationsbyid error " + error)
-  }
-}
+
 
 module.exports = { getClassifications, getInventoryByClassificationId };
