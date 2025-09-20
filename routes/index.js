048d417f0f59572a2867037e63f01f539eb11bf9
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-20 07:33:46 +0100

    index update

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const baseController = require("../controllers/baseController")
 
 // Define route
-router.get("/", baseController.buildHome)
-
+router.get("/intentional-error", (req, res, next) => {
+    const err = new Error("Intentional server error for testing purposes");
+    err.status = 500;
+    next(err);
+});
 module.exports = router
