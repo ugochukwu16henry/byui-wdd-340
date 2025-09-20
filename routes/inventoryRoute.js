433c0ff32ad6910c5d19e10974fb56bd9090ba7a
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-20 13:59:30 +0100

    commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,9 +1,10 @@
 // Needed Resources
 const express = require("express");
 const router =  express.Router();
-const invController = require("../controllers/invController");
+const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/inventory", invController.buildInventory, invController.showInventoryDetail);
-
+router.get("/type/:classificationId", invController.buildByClassificationId)
+// Show a single vehicle by detail
+router.get("/detail/:invId', invController.buildDetailView")
 module.exports = router;
