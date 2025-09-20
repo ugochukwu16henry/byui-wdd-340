9f039b5076b6731eeaca9c688ac34afbafe2cac1
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-20 14:06:30 +0100

    undate

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,66 +1,42 @@
 const invModel = require("../models/inventory-model");
-const utilities = require("../utilities/");
-const { buildVehicleDetailHTML } = require("../utilities/index"); // adjust the path/** */
-const inventoryModel = (req, res) => {
-  res.send("Inventory Paged");
-};
-const buildInventory = (req, res) => {
-  res.send(" Inventory page works!");
-}
-const invCont = {};
-/**
- * GET /inventory/:inv_id
- */
-async function showInventoryDetail(req, res, next) {
+const utilities = require("../utilities");
+
+// Classification view
+async function buildByClassificationId(req, res, next) {
   try {
-    const invId = Number(req.params.inv_id);
-    if (Number.isNaN(invId)) {
-      // invalid id -> 404 via next
-      const err = new Error('Inventory id is invalid');
-      err.status = 404;
-      throw err;
-    }
-
-    const vehicle = await inventoryModel.getInventoryById(invId);
-    if (!vehicle) {
-      const err = new Error('Vehicle not found');
-      err.status = 404;
-      throw err;
-    }
-
-    const vehicleHTML = buildVehicleDetailHTML(vehicle);
-
-    // render the detail view and pass data
-    res.render('inventory/detail', {
-      title: `${vehicle.inv_make} ${vehicle.inv_model} - Details`,
-      vehicle,
-      vehicleHTML, // if you choose to inject HTML string into view
+    const classificationId = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(
+      classificationId
+    );
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className =
+      data.length > 0 ? data[0].classification_name : "Vehicles";
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
     });
   } catch (err) {
-    // forward to error-handling middleware
     next(err);
   }
 }
 
-/* ***************************
- *  Build inventory by classification view
- * ************************** */
-invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId;
-  const data = await invModel.getInventoryByClassificationId(classification_id);
-  const grid = await utilities.buildClassificationGrid(data);
-  let nav = await utilities.getNav();
-  const className = data[0].classification_name;
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  });
-};
+// Detail view
+async function buildDetailView(req, res, next) {
+  try {
+    const invId = req.params.invId;
+    const data = await invModel.getVehicleById(invId);
+    let nav = await utilities.getNav();
+    const detail = utilities.buildVehicleDetail(data);
+    res.render("./inventory/detail", {
+      title: `${data.inv_make} ${data.inv_model}`,
+      nav,
+      detail,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
 
-module.exports = {
-  invCont,
-  showInventoryDetail, buildInventory,
-  // Add buildInventory here if it exists and should be exported
-  // buildInventory
-};
+module.exports = { buildByClassificationId, buildDetailView };
