ca247667b822b0370edfebe28da5fefc87e92cd0
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-22 11:32:53 +0100

    new add

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const router = new express.Router();
+const router = express.Router();
 const inventoryController = require("../controllers/inventoryController");
-const app = express()
-// other middleware here...
-// bring in the inventory routes
-const inventoryRoute = require("../routes/inventoryRoute")
-app.use("/inv", inventoryRoute)  // 👈 this is important
+
 // Route for classification view
 router.get(
   "/type/:classificationId",
   inventoryController.buildByClassificationId
 );
+
+// Route for detail view
+router.get("/detail/:inv_id", inventoryController.buildDetail);
+
 module.exports = router;
