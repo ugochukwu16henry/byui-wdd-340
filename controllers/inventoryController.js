5b7dbc57b8a892d607ed756b3980d1dca68b754c
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-22 11:31:33 +0100

    NEW CHANGE

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,31 +1,54 @@
-const invModel = require("../models/inventory-model");
-const utilities = require("../utilities"); // for nav builder
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities")
 
 /* Build inventory by classification */
 async function buildByClassificationId(req, res, next) {
   try {
-    const classification_id = req.params.classificationId;
-    const data = await invModel.getInventoryByClassificationId(
-      classification_id
-    );
-    let nav = await utilities.getNav();
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    let nav = await utilities.getNav()
 
     if (data.length === 0) {
-      return res.status(404).render("errors/error", {
+      return res.status(404).render("error", {
         title: "Not Found",
         message: "No vehicles found for this classification.",
         nav,
-      });
+      })
     }
 
     res.render("inventory/classification", {
       title: data[0].classification_name + " Vehicles",
       nav,
       vehicles: data,
-    });
+    })
   } catch (err) {
-    next(err);
+    next(err)
   }
 }
 
-module.exports = { buildByClassificationId };
+/* Build detail view for a specific vehicle */
+async function buildDetail(req, res, next) {
+  try {
+    const inv_id = req.params.inv_id
+    const vehicle = await invModel.getVehicleById(inv_id)
+    let nav = await utilities.getNav()
+
+    if (!vehicle) {
+      return res.status(404).render("error", {
+        title: "Vehicle Not Found",
+        message: "Sorry, we could not find that vehicle.",
+        nav,
+      })
+    }
+
+    res.render("inventory/detail", {
+      title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+      nav,
+      vehicle,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = { buildByClassificationId, buildDetail }
