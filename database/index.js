0cca88e07c39b87987a70e61c418b73125cb5d24
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-26 10:02:36 +0100

    commit

--- database/index.js
+++ database/index.js
@@ -1,8 +1,13 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+/* *************
+ * Connection Pool
+ * https://node-postgres.com/api/pool
+ * NOTE: When connecting to a Render PostgreSQL database,
+ * you will need to set the ssl option to true for production connections
+ * ************ */
 let pool;
-
 if (process.env.NODE_ENV === "development") {
   // Development: local testing
   pool = new Pool({
@@ -11,35 +16,32 @@ if (process.env.NODE_ENV === "development") {
       rejectUnauthorized: false,
     },
   });
-
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
-        console.log("executed query", { text });
-        return res;
-      } catch (error) {
-        console.error("error in query", { text, error });
-        throw error;
-      }
-    },
-  };
 } else {
   // Production: Render
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  });
-
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
-        console.log("executed query", { text });
-        return res;
-      } catch (error) {
-        console.error("error in query", { text, error });
-        throw error;
-      }
+    ssl: {
+      rejectUnauthorized: false,
     },
-  };
+  });
 }
+
+// Logs the connection to the database
+if (process.env.NODE_ENV === "development") {
+  pool.on("connect", () => {
+    console.log("PostgreSQL connected (development)");
+  });
+}
+
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params);
+      console.log("executed query", { text: text.slice(0, 100) });
+      return res;
+    } catch (error) {
+      console.error("error in query", { text, error });
+      throw error;
+    }
+  },
+};
