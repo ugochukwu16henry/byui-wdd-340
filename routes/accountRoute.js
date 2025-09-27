cf3635918b245ca3ae091904b2a748346899dc2f
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-27 18:24:22 +0100

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,18 @@ const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 const regValidate = require('../utilities/account-validation')
 
+router.get(
+  "/",
+  utilities.handleErrors(async (req, res) => {
+    const nav = await utilities.getNav();
+    res.render("account/account", {
+      title: "My Account",
+      nav,
+    });
+  })
+);
+
+
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 router.post("/register", utilities.handleErrors(accountController.registerAccount))
