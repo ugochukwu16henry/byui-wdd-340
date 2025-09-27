3d6ad95dd435b15c5624036068cee123fddecc4a
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-27 22:51:05 +0100

    new commit

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
 
@@ -18,22 +19,74 @@ async function buildRegister(req, res, next) {
 }
 
 async function registerAccount(req, res, next) {
-  const nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+  const nav = await utilities.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
 
-  if (!account_firstname || !account_lastname || !account_email || !account_password) {
-    req.flash("notice", "All fields are required.")
-    return res.render("account/registration", { title: "Register", nav, account_firstname, account_lastname, account_email })
+  if (
+    !account_firstname ||
+    !account_lastname ||
+    !account_email ||
+    !account_password
+  ) {
+    req.flash("notice", "All fields are required.");
+    return res.render("account/registration", {
+      title: "Register",
+      nav,
+      account_firstname,
+      account_lastname,
+      account_email,
+    });
   }
 
-  const regResult = await accountModel.registerAccount(account_firstname, account_lastname, account_email, account_password)
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      "notice",
+      "Sorry, there was an error processing the registration."
+    );
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    });
+  }
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    hashedPassword
+  );
 
   if (regResult) {
-    req.flash("notice", `Congratulations, ${account_firstname}, you're registered! Please log in.`)
-    res.status(201).render("account/login", { title: "Login", nav })
+    req.flash(
+      "notice",
+      `Congratulations, ${account_firstname}, you're registered! Please log in.`
+    );
+    res.status(201).render("account/login", { title: "Login", nav });
   } else {
-    req.flash("notice", "Registration failed. Please check your details and try again.")
-    res.status(501).render("account/registration", { title: "Register", nav, account_firstname, account_lastname, account_email })
+    req.flash(
+      "notice",
+      "Registration failed. Please check your details and try again."
+    );
+    res
+      .status(501)
+      .render("account/registration", {
+        title: "Register",
+        nav,
+        account_firstname,
+        account_lastname,
+        account_email,
+      });
   }
 }
 
