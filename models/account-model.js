d1166c8af6b2640096cd20bfb06235e025653e0f
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-27 22:23:45 +0100

    commit

--- models/account-model.js
+++ models/account-model.js
@@ -27,4 +27,17 @@ accountModel.registerAccount = async function (
   }
 };
 
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email){
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1"
+    const email = await pool.query(sql, [account_email])
+    return email.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
+
 module.exports = accountModel;
