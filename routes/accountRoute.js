d2bb1e992b32d8dc4da78e01e90aaf4fcd199764
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-29 09:39:45 +0100

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,32 +1,29 @@
-const express = require("express")
-const router = express.Router()
-const utilities = require("../utilities")
-const accountController = require("../controllers/accountController")
-const regValidate = require('../utilities/account-validation')
+const express = require("express");
+const router = express.Router();
+const utilities = require("../utilities");
+const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get(
-  "/",
-  utilities.handleErrors(async (req, res) => {
-    const nav = await utilities.getNav();
-    res.render("account/account", {
-      title: "My Account",
-      nav,
-    });
-  })
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
 );
-
-
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+router.post(
+  "/register",
+  utilities.handleErrors(accountController.registerAccount)
+);
 // Process the registration data
 router.post(
   "/register",
   regValidate.registationRules(),
   regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
-)
+);
 
-module.exports = router
+// Process the login attempt
+router.post("/login", (req, res) => {
+  res.status(200).send("login process");
+});
 
-
+module.exports = router;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,15 +1,25 @@
-// Needed Resources
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 
-// Route to build inventory by classification view
+// Management view
+router.get("/", invController.buildManagement);
+
+// Add Classification
+router.get("/add-classification", invController.buildAddClassification);
+router.post("/add-classification", invController.addClassification);
+
+// Add Inventory
+router.get("/add-inventory", invController.buildAddInventory);
+router.post("/add-inventory", invController.addInventory);
+
+// Inventory by classification
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
-// Route to build inventory item detail view
+// Inventory detail
 router.get("/detail/:inv_id", invController.buildDetailView);
 
-// Footer error test route
+// Footer error test
 router.get("/error-test", invController.triggerError);
 
 module.exports = router;
