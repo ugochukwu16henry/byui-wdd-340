fd42944576a2ea35b8ba134d35147daf30076eda
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-10-11 06:37:08 +0100

    commit

--- database/index.js
+++ database/index.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV === "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
+      require: true,
       rejectUnauthorized: false,
     },
   });
